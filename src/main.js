016781ce2abbe381d8a0dad423b5d9156664e54e
ted <theodore.cackowski@gmail.com>
2018-08-19 10:28:03 -0500

    going to delete commented code

--- src/main.js
+++ src/main.js
@@ -1,153 +1,138 @@
 
-l('modules loading');
 
 
          pc('parsers')         .then(function(p){
   return pc('blips')           .then(function(b){
     l('resolved parsers');
     l('resolved blips');
-    l('modules loaded');
-  }); }); 
+  }); });
 
-/*
- *
-
-
-
-
-
-var SerialPort = require('serialport');
-//Detecting open errors can be moved to the constructor's callback.
-var port = new SerialPort('/dev/ttyUSB0',{baudRate:9600}, function (err) {
-  if (err) {
-    return console.log('Error: ', err.message);
-  }
-  console.log('Port created: ', arguments);
-});
-
-//Open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('on Error: ', err.message);
-})
-
-
-////When disabling the autoOpen option you'll need to open the port on your own.
-//var SerialPort = require('serialport');
-//var port = new SerialPort('/dev/tty-usbserial1', { autoOpen: false });
-//port.open(function (err) {
-//  if (err) {
-//    return console.log('Error opening port: ', arguments);
-//  }
-//  //// Because there's no callback to write, write errors will be emitted on the port:
-//  ////port.write('hi ');
-//});
-// The open event is always emitted
-port.on('open', function(d) {
-  console.log('Open:', arguments);
-
-  ////Get updates of new data from the serial port as follows:
-  //// Switches the port into "flowing mode"
-  //port.on('data', function (data) {
-  //  console.log('Data:', arguments);
-  //});
-  // Read data that is available but keep the stream from entering "flowing mode"
-  //port.on('readable', function () {
-  //  console.log('Readable:', port.read());
-  //});
-
-});
-
-var Readline = SerialPort.parsers.Readline;
-var parser = new Readline();
-port.pipe(parser);
-parser.on('data', function (data) {
-  console.log('Data:', arguments);
-});
-
-port.write('hi ', function(err) {
-  if (err) {
-    return console.log('Error on write: ', err.message);
-  }
-  console.log('message written',arguments);
-});
-
-
-
-
- *
- *
- * */
 
 ;(function(){
-  root.nodeRequireToPromise = function(moduleName){
+  p.fromNodeModule = function(moduleName){
     //From imperative to functional; just beautiful!
     var nodeModule = require(moduleName);
-    pc(moduleName,p(nodeModule));
+    return p(nodeModule);
   };
-  nodeRequireToPromise('fs');
+  pc.fromNodeModule = function(moduleName){
+    //From imperative to functional; just beautiful!
+    var nodeModulePromise = p.fromNodeModule(moduleName);
+    pc(moduleName,nodeModulePromise);
+    return pc(moduleName);
+  };
+  //Borrowing verbiage from 'q' library
+  root.deNodeify = function(fn){
+    var result = function(){
+      var a = Array.prototype.slice.call(arguments);
+      var e = a.shift();
+      if(e) l('Error: ',e);
+      return fn.apply(this,a);
+    };
+    return result;
+  }
 })();
 
+
 ;(function(){
+  //var SerialPort = require('serialport');
+         pc.fromNodeModule('serialport').then(function(SerialPort){
+  return pc('blips')                    .then(function(b){
+    var d = root.deNodeify;
+    //Detecting open errors can be moved to the constructor's callback.
+    var port = '/dev/ttyUSB0';
+    var baud = 9600;
+    var port = new SerialPort(port,{baudRate:baud,autoOpen:false});
+//    var eventToBlip = function(port,eventName){
+//      var result = new b();
+//      port.on(eventName, function(val) {
+//        l('on '+eventName+': ', arguments );
+//        result.set(val);
+//      });
+//      return result;
+//    };
+    var o = port.open(d(function () {
+      l('Port created: ', arguments);
 
-  //Client module
-  l('running main');
+//      var err = eventToBlip(port,'error');
+//      err.calls(function(val){ l('errBlip: ',arguments); });
+//      var op = eventToBlip(port,'open');
+//      op.calls(function(val){ l('openBlip: ',arguments); });
 
-         pc('fs')    .then(function(fs){
-  return pc('blips') .then(function(b){
+      var parser = new SerialPort.parsers.Readline();
+      port.pipe(parser);
 
-    l('resolved deps');
+//      var dat = eventToBlip(parser,'data');
+//      dat.calls(function(val){ l('dataBlip: ',arguments); });
 
-    b.fileData = function(fileName){
-      var fd = new b();
-      fs
-        .createReadStream(fileName)
-        .on('data',function(err,data){
-          fd.set(err || data);
-        });
-      return fd;
-    };
-    var tty = b.fileData('/dev/ttyUSB0');
-    var str = tty
-      .map(function(dat){
-        var str = dat.toString();
-        return str;
+      parser.on('data', function (data) {
+        l('Data:', arguments);
+        if(data.includes('!')){
+          port.close(d(function() {
+            l('port closed: ',arguments);
+          }));
+        }
       });
-    str
-      .calls(function(dat){
-        l('str: ',dat);
-        var pass = dat.includes('Hello World!');
-        l('pass: ',pass);
-      });
-    b.fileWrite = function(fileName){
-      var fd = new b();
-      var fsOut = fs.createWriteStream(fileName)
-        ;
-      fd.calls(function(d){
-        fsOut.write(d);
-      });
-      return fd;
-    };
-    var out = b.fileWrite('/dev/ttyUSB0');
-
-    out.set('dsp ');
-    //out.set('s');
-    //out.set('p');
-    //out.set(' ');
-    
-
-    //out.set('h');
-    //out.set('i');
-    //out.set(' ');
-
-    //out.set('o');
-    //out.set('n');
-    //out.set('e');
-    //out.set(' ');
-
-    //out.set('d');
-    //out.set('s');
-    //out.set('p');
-    //out.set(' ');
+      port.write('hi ', d(function() {
+        l('message written',arguments);
+      }));
+    port.on('error', function() { l('on Error: ', arguments ); })
+    port.on('open', function() { l('Open:', arguments); });
+    }));
   }); });
 
 })();
+
+
+//;(function(){
+//  //Client module
+//  var pcfs = pc.fromNodeModule('fs');
+//         pcfs        .then(function(fs){
+//  return pc('blips') .then(function(b){
+//    b.fileData = function(fileName){
+//      var fd = new b();
+//      fs
+//        .createReadStream(fileName)
+//        .on('data',function(err,data){
+//          fd.set(err || data);
+//        });
+//      return fd;
+//    };
+//    var tty = b.fileData('/dev/ttyUSB0');
+//    var str = tty
+//      .map(function(dat){
+//        var str = dat.toString();
+//        return str;
+//      });
+//    str
+//      .calls(function(dat){
+//        l('str: ',dat);
+//        var pass = dat.includes('Hello World!');
+//        l('pass: ',pass);
+//      });
+//    b.fileWrite = function(fileName){
+//      var fd = new b();
+//      var fsOut = fs.createWriteStream(fileName)
+//        ;
+//      fd.calls(function(d){
+//        fsOut.write(d);
+//      });
+//      return fd;
+//    };
+//    var out = b.fileWrite('/dev/ttyUSB0');
+//    out.set('dsp ');
+//    //out.set('s');
+//    //out.set('p');
+//    //out.set(' ');
+//    //out.set('h');
+//    //out.set('i');
+//    //out.set(' ');
+//    //out.set('o');
+//    //out.set('n');
+//    //out.set('e');
+//    //out.set(' ');
+//    //out.set('d');
+//    //out.set('s');
+//    //out.set('p');
+//    //out.set(' ');
+//  }); });
+//})();
